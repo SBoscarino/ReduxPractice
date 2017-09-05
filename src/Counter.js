6c67d279745201edb441fb7d83f5251e61208a28
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-09-05 10:34:13 -0500

    destruction button

--- src/Counter.js
+++ src/Counter.js
@@ -8,6 +8,7 @@ function Counter(props) {
       <h1>Counter</h1>
       <p>Count: {props.count}</p>
       <button onClick={props.onIncrementClick}>Add One!</button>
+      <button onClick={props.onDecrementClick}>DESTROY One!</button>
     </div>
   )
 }
@@ -19,8 +20,14 @@ function mapStateToProps(state){
   }
 }
 
-function mapDispatchToPropr(dispatch) {
+function mapDispatchToProp(dispatch) {
   return {
+    onDecrementClick: () => {
+      console.log('clicked DESTRUCTION');
+      const action = {type: "DECREMENT"};
+      dispatch(action);
+    },
+
     onIncrementClick: () => {
       console.log('clicky tha button');
       const action = { type: "INCREMENT"};
@@ -29,5 +36,5 @@ function mapDispatchToPropr(dispatch) {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToPropr)(Counter);
+export default connect(mapStateToProps, mapDispatchToProp)(Counter);
 //wrap our component in their connect component and glue them together.
